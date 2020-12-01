49e33605238d0cba7af9215d9a6a95d27ddfb93f
JT-Potato <joel123droid@outlook.com>
2020-12-01 21:06:20 +1100

    Commit before major redesign

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 import os
+from vidHelperTools import *
 
 class App():
     def __init__(self, master):
         self.master = master
-        master.title("Vid Helper")
-
-        #Labels
-        self.label = tk.Label(master, text="Welcome")
-        self.label.pack()
+        master.title("Workflow One")
+        #Splash
+        canvas  = tk.Canvas(tk.Tk(), f"{getWidth()}x{getHeight()}")
+        canvas.create_text(50, 50, text="Welcome to Workflow One")
+        canvas.pack()
+        tk.update()
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import platform
 import tkinter as tk
+import tkinter.font as TkFont
 import os
 import gui
 from vidHelperTools import *
 
 root = tk.Tk()
-text = tk.Text(master=self.frame)
-text.configure(font-"Inter")
+print(TkFont.families())
+root.option_add("*Font", "Inter 40")
 root.geometry(f"{getWidth()}x{getHeight()}")
 if platform.system() == "Darwin":
     os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
-    gui.App(root)
+root.title("Workflow One")
+#Splash
+canvas  = tk.Canvas(root, width=getWidth(), height=getHeight())
+canvas.create_text(int(getWidth())/2, int(getHeight())/2, text="Welcome to Workflow One")
+canvas.pack()
 root.mainloop()
