dfd1c9156cec948e5719ece316950eb030f715cc
JT-Potato <joel123droid@outlook.com>
2020-12-01 21:11:39 +1100

    Using web instead of tk for UI

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to Workflow One!
+This version of Workflow One (ALPHA-1.0.0) is called Workflow One Dandenong.
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import platform
-import tkinter as tk
-import tkinter.font as TkFont
-import os
-import gui
-from vidHelperTools import *
+from flaskwebgui import FlaskUI
+from flask import *
 
-root = tk.Tk()
-print(TkFont.families())
-root.option_add("*Font", "Inter 40")
-root.geometry(f"{getWidth()}x{getHeight()}")
-if platform.system() == "Darwin":
-    os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
-root.title("Workflow One")
-#Splash
-canvas  = tk.Canvas(root, width=getWidth(), height=getHeight())
-canvas.create_text(int(getWidth())/2, int(getHeight())/2, text="Welcome to Workflow One")
-canvas.pack()
-root.mainloop()
+app = Flask(__name__)
+ui = FlaskUI(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+ui.run()
