7e777daa9d45d5ac5179ae27e886466ed1a858ab
JT-Potato <joel123droid@outlook.com>
2020-12-01 20:24:20 +1100

    Changed fonts to Inter

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import platform
 import tkinter as tk
 import os
-import macOS
+import gui
 from vidHelperTools import *
 
 root = tk.Tk()
+text = tk.Text(master=self.frame)
+text.configure(font-"Inter")
 root.geometry(f"{getWidth()}x{getHeight()}")
 if platform.system() == "Darwin":
     os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
-    gui = macOS.App(root)
+    gui.App(root)
 root.mainloop()
