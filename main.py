a02723afb11d27bbdab70a63db68f25b2903de58
JT-Potato <joel123droid@outlook.com>
2020-12-14 09:21:21 +1100

    Repairing mistakes

--- main.py
+++ main.py
@@ -2,35 +2,16 @@ import webview
 from flask import *
 import threading
 import os
-import platform
-import json
-import vidHelperTools
-from vidHelperTools import config
+import config
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '0'
 app = Flask(__name__)
-app.config["workflowconfig"] = vidHelperTools.default_config
+
+config.repair()
 
 @app.route("/")
-def index():
-    if platform.system() == "Darwin":
-        if os.path.isfile(os.path.expanduser(vidHelperTools.config_file)) == False:
-            os.makedirs(os.path.expanduser(vidHelperTools.config_folder))
-            config.save(app.config["workflowconfig"])
-        else:
-            app.config["workflowconfig"] = config.load()
-    if vidHelperTools.repairConfig() == "repaired":
-        return render_template("something_went_wrong.html")
-    return render_template("index.html", data=app.config["workflowconfig"])
-
-@app.route("/internal/<action>/<extra>")
-def internal(action, extra):
-    if action == "mkproj":
-        if extra != "none":
-            os.makedirs(os.path.expanduser(app.config["workflowconfig"]["default_proj_loc"]) + "/" + extra)
-            return render_template("success.html", message="Your Project Has Been Created!")
-        else:
-            return render_template("require_response.html", question="What is the name of your project?", return_loc="/internal/mkproj/")
+def home():
+    return render_template("index.html", wallpaper=config.read()["wallpaper"])
 
 def run():
     app.run()
