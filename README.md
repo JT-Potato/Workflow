36d766852db7e44021065a8b423be9856608bdcf
JT-Potato <joel123droid@outlook.com>
2020-12-15 15:15:23 +1100

    Added project creation

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # About
 This project is designed to aid the workflows of smaller, more flexible content creators, rather than people at a commercial level. More functionality regarding the needs of commerical users is currently planned.
 
+# File management
+The folder you select to create your project in is the Project Folder.
+
 # Developer
 See developer docs at DEVELOPER.md
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+var fs = require('fs');
+var path = require('path');
+var readline = require('readline-sync');
+const notifier = require('node-notifier');
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -26,12 +30,27 @@ app.on('activate', () => {
 })
 
 ipcMain.on('makeproj', (event, arg) => {
-        dialog.showOpenDialog({properties: ['openDirectory']}).then(
-            result => {
-                var filepath = result.filepaths
-                if (typeof filepath !== 'undefined' && filepath.length > 0) {
-                    //idk
-                }
+    dialog.showOpenDialog({properties: ['openDirectory']}).then(
+        result => {
+            var filepath = result.filePaths
+            if (typeof filepath !== 'undefined' && filepath.length > 0) {
+                filepath = filepath[0]
+                var arr = filepath.split('/');
+                var name = arr[arr.length-1] || arr[arr.length-2];
+                console.log(filepath)
+                console.log(name)
+                let project = {"name": name}
+                project = JSON.stringify(project);
+                fs.writeFileSync(path.join(filepath, 'project.json'), project)
+                notifier.notify({
+                    title: 'Workflow',
+                    message: 'Project Created Successfully'
+                });
             }
-        )
+        }
+    )
 });
+
+ipcMain.on('settings', (event, arg) => {
+    //idk
+})
