b680def3893475254a83cfd5ad250d1fc680e396
JT-Potato <joel123droid@outlook.com>
2021-01-15 19:49:27 +1100

    Last commit before properly using github

---
+++ internal/channelListeners.js
@@ -0,0 +1,40 @@
+var backend = require('./backend_functions')
+var fs = require('fs');
+var path = require('path');
+const notifier = require('node-notifier');
+const { dialog } = require('electron');
+
+var registerHandlers = function(ipcMain) {
+    ipcMain.on('openproj', (event, arg) => {
+        dialog.showOpenDialog({properties: ['openDirectory']}).then(
+            result => {
+                var filepath = result.filePaths
+                if (backend.filepathCheck(filepath)) {
+                    console.log(filepath)
+                }
+            }
+        )
+    })
+    ipcMain.on('makeproj', (event, arg) => {
+        dialog.showOpenDialog({properties: ['openDirectory']}).then(
+            result => {
+                var filepath = result.filePaths
+                if (backend.filepathCheck(filepath)) {
+                    filepath = filepath[0]
+                    var arr = filepath.split('/');
+                    var name = arr[arr.length-1] || arr[arr.length-2];
+                    console.log(filepath)
+                    console.log(name)
+                    let project = {"name": name}
+                    project = JSON.stringify(project);
+                    fs.writeFileSync(path.join(filepath, 'project.json'), project)
+                    notifier.notify({
+                        title: 'Workflow',
+                        message: 'Project Created Successfully'
+                    });
+                }
+            }
+        )
+    });
+}
+module.exports = registerHandlers
--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-var fs = require('fs');
-var path = require('path');
-const notifier = require('node-notifier');
-var backend = require('./internal/backend_functions')
+var registerHandlers = require('./internal/channelListeners')
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
-    width: 1920,
-    height: 1080,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true
@@ -17,7 +12,10 @@ function createWindow () {
   win.loadFile('html/index.html')
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(function() {
+  registerHandlers(ipcMain)
+  createWindow()
+})
 
 app.on('window-all-closed', () => {
     app.quit()
@@ -27,41 +25,4 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
-
-ipcMain.on('makeproj', (event, arg) => {
-    dialog.showOpenDialog({properties: ['openDirectory']}).then(
-        result => {
-            var filepath = result.filePaths
-            if (backend.filepathCheck(filepath)) {
-                filepath = filepath[0]
-                var arr = filepath.split('/');
-                var name = arr[arr.length-1] || arr[arr.length-2];
-                console.log(filepath)
-                console.log(name)
-                let project = {"name": name}
-                project = JSON.stringify(project);
-                fs.writeFileSync(path.join(filepath, 'project.json'), project)
-                notifier.notify({
-                    title: 'Workflow',
-                    message: 'Project Created Successfully'
-                });
-            }
-        }
-    )
-});
-
-ipcMain.on('openproj', (event, arg) => {
-    dialog.showOpenDialog({properties: ['openDirectory']}).then(
-        result => {
-            var filepath = result.filePaths
-            if (backend.filepathCheck(filepath)) {
-                
-            }
-        }
-    )
-})
-
-ipcMain.on('settings', (event, arg) => {
-    //idk
 })
