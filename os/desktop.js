3c4cd595c196bfc972276676c9e9907bd4988976
JT-Potato <joel123droid@outlook.com>
2021-02-01 20:27:21 +1100

    Added dock

--- os/desktop.js
+++ os/desktop.js
@@ -1,8 +1,10 @@
+const fs = require('fs')
 var desktop = document.createElement("div")
 document.getElementById("body").appendChild(desktop)
 var config = {
                 "wallpaper": "os/desktop/Brighton/rock4.jpg",
-                "dockHeight": "5"
+                "dockHeight": "50",
+                "blur": "5"
             } //replace with config from cookiieee
 desktop.setAttribute(
                         "style",
@@ -15,12 +17,35 @@ desktop.setAttribute(
                     )
 
 //Dock
+var dockParent = document.createElement("div")
+dockParent.setAttribute("style", "height: 100vh; width: 100vw; display: flex; justify-content: center;")
+desktop.appendChild(dockParent)
 var dock  = document.createElement("div")
-desktop.appendChild(dock)
+dockParent.appendChild(dock)
 dock.setAttribute(
                     "style",
-                    `background-color: rgba(170, 170, 170, 0.4);
-                    height: ${config["dockHeight"]}%;
-                    width: 100vw;
-                    position: fixed;`
+                    `background-color: rgba(40, 40, 40, 0.4);
+                    backdrop-filter: blur(${config["blur"]}px);
+                    height: ${config["dockHeight"]}px;
+                    position: absolute;
+                    bottom: 0;
+                    display: flex;
+                    justify-content: center;
+                    padding-left: 10px;
+                    padding-right: 10px;`
                 )
+//dock icons
+var dockIcons
+try {
+    const jsonString = fs.readFileSync('os/dock.json')
+    dockIcons = JSON.parse(jsonString)
+  } 
+catch(err) {
+    console.log(err)
+}
+dockIcons.forEach(function(item, index) {
+    var icon = document.createElement("img")
+    icon.setAttribute("style", `height: ${config["dockHeight"]}px; margin-left: 5px; margin-right: 5px;`)
+    icon.src = item["icon"]
+    dock.appendChild(icon)
+})
