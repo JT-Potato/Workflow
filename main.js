75cb0528713ddadffa36db65a173e88d1610ead4
JT-Potato <joel123droid@outlook.com>
2021-04-30 18:05:07 +1000

    Archiving junk code. Returning to more HTML

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-var registerHandlers = require('./internal/channelListeners')
-var devCfg =  require("./boot/devSecretConfig")
+require('v8-compile-cache');
+
+var registerHandlers = require('./internal/channelListeners') //Event handlers
+var devCfg =  require("./boot/devSecretConfig") //Secret developer configs
+
+//Settings for the Nodejs window.
 function createWindow() {
   const win = new BrowserWindow({
     webPreferences: {
@@ -8,10 +12,13 @@ function createWindow() {
       enableRemoteModule: true
     }
   })
+
+  //Set whether or not this is a production app
   if(devCfg.production == true) {
     win.removeMenu()
     console.log("Workflow: Is Production App")
   }
+  //Loads index.html - the foundation of our app
   win.loadFile('index.html')
 }
 
@@ -20,10 +27,13 @@ app.whenReady().then(function() {
   createWindow()
 })
 
+//Quits the app when all windows are closed.
+//Yes this violates Apple's HIG, but do I really care?
 app.on('window-all-closed', () => {
     app.quit()
 })
 
+//Starts the app
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
